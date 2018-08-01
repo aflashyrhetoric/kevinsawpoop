6dbc013d935241d30ab65cd3880512e0999829da
Kevin Oh <aflashyrhetoric@gmail.com>
2018-08-01 17:51:00 -0400

    Quick deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,12 @@
-vue build
-cp _redirects dist/_redirects
-git add -A 
-git commit -m "Quick deploy"
-git push origin master
+echo "Building...\n"
+vue build;
+
+echo "Copying redirects...\n"
+cp _redirects dist/_redirects;
+
+echo "Committing to git\n"
+git add -A;
+git commit -m "Quick deploy";
+git push origin master;
+
+echo "\n ------> Done."
